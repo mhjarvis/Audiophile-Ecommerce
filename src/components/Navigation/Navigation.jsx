93380a243a71f5b8ffddbb38ed35b529cf0ddfc2
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-12 13:59:25 -0500

    Add asset folder and old html mockups

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,13 @@
 import "./Navigation.css"
 
 export default function Navigation() {
-	return <div> HEllo</div>
+	return (
+		<section>
+			<div className="navigation__container">
+				<p className="navigation__logo">audiophile</p>
+				<div className="navigation__hamburger-menu">---</div>
+				<button className="navigation__cart">🛒</button>
+			</div>
+		</section>
+	)
 }
