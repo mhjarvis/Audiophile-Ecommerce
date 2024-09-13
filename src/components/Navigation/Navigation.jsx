0dbc84acbc247c82c990a90f1fe862dafeb8ec36
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-12 21:58:09 -0500

    Add in navigation element

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,19 @@
 import "./Navigation.css"
+import hamburger from "../../assets/shared/tablet/icon-hamburger.svg"
+import cartImage from "../../assets/shared/desktop/icon-cart.svg"
+import logo from "../../assets/shared/desktop/logo.svg"
 
 export default function Navigation() {
 	return (
-		<section>
+		<section className="navigation__section">
 			<div className="navigation__container">
-				<p className="navigation__logo">audiophile</p>
-				<div className="navigation__hamburger-menu">---</div>
-				<button className="navigation__cart">🛒</button>
+				<img src={hamburger} className="navigation__hamburger-menu"></img>
+				<img src={logo} alt="Audiophile logo" className="navigation__logo" />
+				<img
+					src={cartImage}
+					alt="Cart image"
+					className="navigation__cart"
+				></img>
 			</div>
 		</section>
 	)
