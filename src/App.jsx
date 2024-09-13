d0287dc3bd1b975186cb91e1f80dd464bf339329
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-12 22:14:35 -0500

    Create header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Navigation from "./components/Navigation/Navigation"
+import Header from "./components/Header/Header"
 
 export default function App() {
 	return (
 		<div>
 			<Navigation />
+			<Header />
 		</div>
 	)
 }
