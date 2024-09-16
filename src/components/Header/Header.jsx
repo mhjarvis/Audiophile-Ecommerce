dc2f2a67d4d31e2a9f43d4e7cde5bd2712ebe1b6
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-16 13:53:37 -0500

    Update header for 1048px and smaller

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,23 @@ import headphonesDesktop from "../../assets/home/desktop/image-hero.jpg";
 
 export default function Header() {
 	return (
-		<section className="header__section">
+		<div className="header__section">
+			{/* picture element */}
 			<picture className="header__image">
 				<source media="(min-width: 1024px)" srcSet={headphonesDesktop} />
 				<source media="(min-width: 768px)" srcSet={headphonesTablet} />
 				<img src={headphonesMobile} alt="Headphones" />
 			</picture>
-			<div className="header__container"></div>
-		</section>
+			<div className="header__container">
+				<div className="text__container">
+					<h1 className="header__title">XX99 Mark II Headphones</h1>
+					<p className="header__text">
+						Experience natural, lifelike audio and exceptional build quality
+						made for the passionate music enthusiast.
+					</p>
+					<button className="header__button">See Product</button>
+				</div>
+			</div>
+		</div>
 	);
 }
