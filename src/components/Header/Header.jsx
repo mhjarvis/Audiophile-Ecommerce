f9a9a3e0b2439de53f54a883b9d965b20d025605
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-15 22:57:54 -0500

    Add image ; needs fixing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,17 @@
-import "./Header.css"
+import "./Header.css";
+import headphonesMobile from "../../assets/home/mobile/image-header.jpg";
+import headphonesTablet from "../../assets/home/tablet/image-header.jpg";
+import headphonesDesktop from "../../assets/home/desktop/image-hero.jpg";
 
 export default function Header() {
-	return <section>hi</section>
+	return (
+		<section className="header__section">
+			<picture className="header__image">
+				<source media="(min-width: 1024px)" srcSet={headphonesDesktop} />
+				<source media="(min-width: 768px)" srcSet={headphonesTablet} />
+				<img src={headphonesMobile} alt="Headphones" />
+			</picture>
+			<div className="header__container"></div>
+		</section>
+	);
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -29,7 +29,6 @@ export default function Navigation() {
 					className="navigation__cart"
 				></img>
 			</div>
-			<div>test</div>
 		</section>
 	);
 }
