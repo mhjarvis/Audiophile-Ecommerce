ec3e05c4450951698d31110183a525c1160fd8e3
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-16 22:43:59 -0500

    Import shopSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css"
-import Navigation from "./components/Navigation/Navigation"
-import Header from "./components/Header/Header"
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Header from "./components/Header/Header";
+import ShopSection from "./components/ShopSelection/ShopSection";
 
 export default function App() {
 	return (
 		<div>
 			<Navigation />
 			<Header />
+			<ShopSection />
 		</div>
-	)
+	);
 }
